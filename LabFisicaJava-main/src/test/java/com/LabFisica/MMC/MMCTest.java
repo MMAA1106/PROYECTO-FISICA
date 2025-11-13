5f7c0a004467e0ea0322fa7d15909239743461a6
MMAA1106 <202500370@est.umss.edu>
2025-11-12 21:04:07 -0400

    Update MMCTest.java

--- LabFisicaJava-main/src/test/java/com/LabFisica/MMC/MMCTest.java
+++ LabFisicaJava-main/src/test/java/com/LabFisica/MMC/MMCTest.java
@@ -20,7 +20,7 @@ public class MMCTest {
         // Cargar el CSV
         var url = getClass().getResource("/tabla1.csv");
         assertNotNull(url, "El archivo CSV debe estar en la ruta de test classpath");
-        f233d0b (Archivo Subido)
+        f233d0b ("Archivo Subido");
         var path = Paths.get(url.toURI());
 
         XYData xy = CsvXYReader.read(path);
